2d2f308626deeffb86329290bb066b9b1dbb974f
RoanMacmillan <hibee94@gmail.com>
2023-01-04 14:41:06 +0000

    mobile menu closes when resizing to desktop

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
 const modal = document.querySelector('.modal');
+let menuOpen = false;
 
-
-function toggleMobileMenu() {
-
-    document.getElementById('mobile-menu').classList.toggle('transition');
-    document.getElementById('open').classList.toggle('close');
-    document.getElementById('close').classList.toggle('open');
-    modal.classList.toggle('modal-transition');
-
-    // Prevents scrolling when mobile menu is open //
-    document.body.classList.toggle('disable');
+// Toggles menu, modal and hamburger icons
+function toggleMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  const openBtn = document.getElementById('open');
+  const closeBtn = document.getElementById('close');
+  mobileMenu.classList.toggle('transition');
+  openBtn.classList.toggle('close');
+  closeBtn.classList.toggle('open');
+  modal.classList.toggle('modal-transition');
 }
 
+// Disables scroll if menu is opened
+function toggleMenuAndBodyScroll(enable) {
+  toggleMenu();
+  if (enable) {
+    document.body.classList.add('disable-scroll');
+  } else {
+    document.body.classList.remove('disable-scroll');
+  }
+}
 
-// Opens and closes mobile menu //
-document.querySelectorAll('.hamburger').forEach((btn) => {
+const hamburgers = document.querySelectorAll('.hamburger');
 
+hamburgers.forEach((btn) => {
     btn.addEventListener('click', () => {
-
-        toggleMobileMenu();      
+            menuOpen = !menuOpen;
+            toggleMenuAndBodyScroll(menuOpen);
+          });
 })
-})
 
-// Close mobile menu when clicking outside //
-modal.addEventListener('click', () => {
 
-    toggleMobileMenu();
-})
+// Close mobile menu when clicking outside 
+if (modal) {
+  modal.addEventListener('click', () => {
+    menuOpen = !menuOpen;
+    toggleMenuAndBodyScroll(menuOpen);
+  });
+}
+
+// Close mobile menu when resizing to a minimum width of 700px
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 700 && menuOpen) {
+    menuOpen = false;
+    toggleMenuAndBodyScroll(false);
+  }
+});
+
+
